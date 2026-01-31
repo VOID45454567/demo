736b95bc13a693350b9d72d18fd43ec2730653a8
kirill <kirill@MacBook-Air-kirill.local>
2026-01-31 14:51:39 +0500

    started slider

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,15 @@
 @section('title', 'Главная')
 @section('content')
     <h1>Тут Главная</h1>
+
+    <div class="slider-section">
+
+        <div class="image" id="image"></div>
+    </div>
+    @push('scripts')
+        <script>
+            const image = document.getElementById('image')
+
+        </script>
+    @endpush
 @endsection
